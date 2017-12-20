a632d1af861ce940cb2d89c07d62df27214299cf
395989356 <33148589+395989356@users.noreply.github.com>
2017-12-20 15:14:33 +0800

    adsf

--- NodeJS/04Buffer/03读取数据.js
+++ NodeJS/04Buffer/03读取数据.js
@@ -5,5 +5,5 @@ for (var index = 0; index < buffer.length; index++) {
 console.log('写入缓冲区字节：' + buffer.length);
 console.log('数据：' + buffer.toString('ascii'));
 console.log('数据：' + buffer.toString('ascii', 0, 6).toUpperCase());
-console.log('数据：' + buffer.toString('utf-8', 0, 6).toUpperCase());
+console.log('数据：' + buffer.toString('utf-8', 0, 6).toUpperCase()); 
 console.log('数据：' + buffer.toString(undefined, 0, 6).toUpperCase());
